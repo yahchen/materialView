a78b45e34c876ca5586785b2f22acb5f7a1e10fe
wangyaohui <wangyaohui@qiyi.com>
2017-06-01 19:50:52 +0800

    repackage

---
+++ coreSyncer/src/main/java/com/hx/syncer/CoreSyncerApplication.java
@@ -0,0 +1,12 @@
+package com.hx.syncer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreSyncerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreSyncerApplication.class, args);
+	}
+}
---
+++ moniterWeb/src/main/java/com/hx/monitor/MoniterWebApplication.java
@@ -0,0 +1,12 @@
+package com.hx.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoniterWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoniterWebApplication.class, args);
+	}
+}
