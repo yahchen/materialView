9ff56860471487d75436d5669bcd9e8cba454005
yahchenyu <yahchenyu@gmail.com>
2018-03-03 17:44:54 +0800

    add script

---
+++ dataSyncer/scripts/rmSiteDataPsql.sql
@@ -0,0 +1,14 @@
+DELETE FROM public.site_data_head
+    WHERE data_logo=test and data_time = 23232
+DELETE FROM public.gm_site_surf_gl
+    WHERE s_d_id=45
+DELETE FROM public.gm_site_temp_gl
+    WHERE s_d_id=45
+DELETE FROM public.gm_site_surf_ch
+    WHERE s_d_id=45
+DELETE FROM public.rm_site_surf_gl
+    WHERE s_d_id=45
+DELETE FROM public.rm_site_temp_gl
+    WHERE s_d_id=45
+DELETE FROM public.rm_site_surf_ch
+    WHERE s_d_id=45
