cc7e09de8ff48e87677232535fdb4706424bf6bd
yahchenyu <yahchenyu@gmail.com>
2018-03-27 00:26:53 +0800

    script opt

--- dataSyncer/scripts/rm-bad-griddata-postgres.sh
+++ dataSyncer/scripts/rm-bad-griddata-postgres.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 export PGPASSWORD=postgres
-sed -i "s/data_logo=[a-zA-Z_\-\:\d\']*/data_logo='$1'/" /data/rmGridDataPsql.sql
-sed -i "s/data_time=[0-9\-\']*/data_time='$2'/" /data/rmGridDataPsql.sql
-sed -i "s/s_d_id=[0-9]*/s_d_id='$3'/" /data/rmGridDataPsql.sql
-psql -U postgres -h 127.0.0.1 -d postgres -f /data/rmGridDataPsql.sql
+sed -i "s/data_logo=[a-zA-Z_\-\:\d\']*/data_logo='$1'/" rmGridDataPsql.sql
+sed -i "s/data_time=[0-9\-\']*/data_time='$2'/" rmGridDataPsql.sql
+psql -U postgres -h 127.0.0.1 -d postgres -f rmGridDataPsql.sql
 
-
-#测试用例：sh rm-bad-griddata-postgres.sh ﻿GM_RSURF_GL '2017-10-11 00:00:00' 25#!/usr/bin/env bash
+#                                         data_logo        data_time
+#测试用例：sh rm-bad-griddata-postgres.sh ﻿GM_GRID_KWBC_GL '2017-10-11 00:00:00'
--- dataSyncer/scripts/rmGridDataPsql.sql
+++ dataSyncer/scripts/rmGridDataPsql.sql
@@ -1,2 +1,2 @@
 DELETE FROM public.grid_data_head
-    WHERE data_logo=test and data_time=23232
+    WHERE data_logo='GM_GRID_KWBC_GL' and data_time='2018-02-20 00:00:00'
